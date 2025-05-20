16c1e300feb1763aab478a6961e96dd08bb4edd0
farza <farzanrichu610@gmai.com>
2025-05-21 02:23:14 +0530

    type conv

---
+++ type_conversion.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+    {
+float a=2.7182818;//this is a float
+cout<<"THis is a floating type num: "<<a<<endl;
+cout<<"THis is a floating type num after type conversion: "<<int(a)<<endl;//now this is int datatype
+    string name="hello";
+    cout<<"Normal String"<<name<<endl;
+    cout<<" String after type conversion using char which is modified by specifying the element positon::"<<char(name[0])<<endl;
+
+return 0;
+}
