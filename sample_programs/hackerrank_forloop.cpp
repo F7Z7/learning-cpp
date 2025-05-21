65a498a45272bbd392184f103c1002ac684eb77f
farza <farzanrichu610@gmai.com>
2025-05-22 01:20:46 +0530

    .

---
+++ sample_programs/hackerrank_forloop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    string arr[9]={ "one","two","three","four","five","six","seven","eight","nine"};
+    int a,b;
+    cout<<"Enter the number of elements you want to enter: "<<endl;
+    cin>>a;
+    cin>>b;
+
+
+
+    return 0;
+}
