fe4f53fc077995ab1a26d5e08c346279a863fb9f
farza <farzanrichu610@gmai.com>
2025-05-22 00:04:10 +0530

    conditons.cpp

---
+++ basics/conditons.cpp
@@ -0,0 +1,14 @@
+  #include<iostream>
+  using namespace std;
+  int main(){
+    //conditons
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    if(a%2==0){
+      cout<<"The entered number "<<a<< " is even"<<endl;
+      }
+      else{
+        cout<<"The entered number" <<a<< "is odd"<<endl;
+      }
+  }
